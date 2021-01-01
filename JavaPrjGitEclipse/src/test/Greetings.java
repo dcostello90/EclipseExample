2c01d5dfa5f4e13d0650dc6478523728b21c38f7
oisin <l00143552@student.lyit.ie>
2021-01-01 20:20:06 +0000

    Made additions to console output.

---
+++ JavaPrjGitEclipse/src/test/Greetings.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("L00143552");
+		System.out.println("Greetings Everyone");
+
+	}
+
+}
